21678edd7286bc69f2594f23efd5ed4c0f567b40
Luighi <luighiavz@gmail.com>
2022-05-30 12:29:16 -0500

    Required packages and configurations to reduce serverless size

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ module.exports = {
   webpack(config, { isServer }) {
     if (isServer) {
       require('./scripts/optimize-svg');
+      config.externals = {
+        bufferutil: 'bufferutil',
+        'utf-8-validate': 'utf-8-validate',
+      };
     }
     config.module.rules.push({
       test: /\.svg$/,
@@ -9,5 +13,11 @@ module.exports = {
     });
 
     return config;
+  },
+  env: {
+    api:
+      process.env.NEXT_PUBLIC_ENVIRONMENT === 'production'
+        ? 'https://api.congreso.openpolitica.com/api/'
+        : 'https://api.dev.congreso.openpolitica.com/api/',
   },
 };
